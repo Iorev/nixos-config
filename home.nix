43c06629bfe3e39f113984f8cd1abf0154de578a
Utisse <lorenzopasqui@gmail.com>
2024-11-03 01:52:39 +0100

    97 current  2024-11-03 01:44:20  24.11.20241029.807e915  6.6.58                          *

--- home.nix
+++ home.nix
@@ -56,6 +56,7 @@
     stremio
     rofi-wayland
     kitty
+    alejandra
   ];
 
   imports = [
@@ -111,8 +112,8 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    ".config/hypr/hyperland.conf".source = ./config/hypr/hyperland.conf;
-    ".config/hypr/start.sh".source = ./config/hypr/start.sh;
+    #".config/hypr/hyperland.conf".source = ./config/hypr/hyperland.conf;
+    #".config/hypr/start.sh".source = ./config/hypr/start.sh;
   };
 
   # Home Manager can also manage your environment variables through
