6d33b8395fb055e0ae4b570e6972a4c9e3642e56
Utisse <lorenzopasqui@gmail.com>
2025-04-15 18:10:40 +0200

    lowered external gaps

--- programs/hyprland.nix
+++ programs/hyprland.nix
@@ -8,7 +8,7 @@
     settings = {
       general = {
         gaps_in = 1;
-        gaps_out = 5;
+        gaps_out = 2;
         border_size = 1;
         #"col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
         #"col.inactive_border" = "rgba(595959aa)";
