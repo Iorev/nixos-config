5e51bac5b8569ce6f1f6aeade0181e8189606a78
Utisse <lorenzopasqui@gmail.com>
2025-04-16 19:50:29 +0200

    changed style

--- programs/stylix.nix
+++ programs/stylix.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: let
   inputImage = ../config/wallpaper/nix-transp.png;
-  themeName = "nord";
+  themeName = "espresso";#https://tinted-theming.github.io/tinted-gallery/
   theme = "${pkgs.base16-schemes}/share/themes/${themeName}.yaml";
   wallpaper = pkgs.runCommand "nix-colored.png" {} ''
     COLOR=$(${pkgs.yq}/bin/yq -r .palette.base00 ${theme})
