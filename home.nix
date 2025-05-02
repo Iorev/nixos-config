24b118df42ca34e785d147ad0ddcfc2a0554855c
Utisse <lorenzopasqui@gmail.com>
2025-05-02 18:56:53 +0200

    Configures zsh and updates aliases
    Configures zsh with powerlevel10k and zoxide.

--- home.nix
+++ home.nix
@@ -2,6 +2,7 @@
   config,
   pkgs,
   inputs,
+  lib,
   ...
 }: let
   inherit (pkgs.stdenv.hostPlatform) system;
@@ -122,18 +123,22 @@ in {
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
-
-    initExtra = ''
+    initContent = ''
+      if [[ -r "${pkgs.zsh-powerlevel10k}/p10k-instant-prompt-finalize.zsh" ]]; then
+        source "\${pkgs.zsh-powerlevel10k}/p10k-instant-prompt-finalize.zsh"
+      fi 
       eval "$(zoxide init --cmd cd zsh)"
+      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
     '';
     oh-my-zsh = {
       enable = true;
       plugins = ["git" "eza" "fzf" "safe-paste" "ssh" "ssh-agent" "zoxide"];
-      theme = "bira";
     };
     shellAliases = {
       ll = "ls -l";
       update = "sudo nixos-rebuild switch --flake /home/lorev/nixos-config/#";
+      updateServer = "sudo nixos-rebuild switch --flake /home/lorev/nixos-homelab/#homelab --target-host root@homelab --verbose";
     };
     history.size = 10000;
   };
