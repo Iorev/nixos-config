7c0b97d0e5bd80dfea83b107616295f92581c17d
lorev <lorenzopasqui@gmail.com>
2025-05-10 15:39:10 +0200

    added borgbase integration to homepage with relative secrets

--- programs/homepage.nix
+++ programs/homepage.nix
@@ -1,6 +1,5 @@
 {
   config,
-  pkgs,
   ...
 }: {
   sops.secrets = {
@@ -8,24 +7,30 @@
     "homepage/ha/url" = {};
     "homepage/tailscale/key" = {};
     "homepage/tailscale/deviceid" = {};
+    "homepage/immich/key" = {};
+    "homepage/immich/url" = {};
     "homepage/gitea/key" = {};
     "homepage/gitea/url" = {};
     "homepage/miniflux/key" = {};
     "homepage/miniflux/url" = {};
     "homepage/uptimekuma/url" = {};
     "homepage/uptimekuma/slug" = {};
+    "homepage/borg/key" = {};
   };
   sops.templates."homepage-dashboard.env".content = ''
     HOMEPAGE_VAR_TAILSCALE_KEY=${config.sops.placeholder."homepage/tailscale/key"}
     HOMEPAGE_VAR_TAILSCALE_DEVICE_ID=${config.sops.placeholder."homepage/tailscale/deviceid"}
     HOMEPAGE_VAR_GITEA_KEY=${config.sops.placeholder."homepage/gitea/key"}
     HOMEPAGE_VAR_GITEA_URL=${config.sops.placeholder."homepage/gitea/url"}
+    HOMEPAGE_VAR_IMMICH_KEY=${config.sops.placeholder."homepage/immich/key"}
+    HOMEPAGE_VAR_IMMICH_URL=${config.sops.placeholder."homepage/immich/url"}
     HOMEPAGE_VAR_MINIFLUX_KEY=${config.sops.placeholder."homepage/miniflux/key"}
     HOMEPAGE_VAR_MINIFLUX_URL=${config.sops.placeholder."homepage/miniflux/url"}
     HOMEPAGE_VAR_HA_KEY=${config.sops.placeholder."homepage/ha/key"}
     HOMEPAGE_VAR_HA_URL=${config.sops.placeholder."homepage/ha/url"}
     HOMEPAGE_VAR_UPTIMEKUMA_URL=${config.sops.placeholder."homepage/uptimekuma/url"}
     HOMEPAGE_VAR_UPTIMEKUMA_SLUG=${config.sops.placeholder."homepage/uptimekuma/slug"}
+    HOMEPAGE_VAR_BORG_KEY=${config.sops.placeholder."homepage/borg/key"}
   '';
 
   services.homepage-dashboard = {
@@ -33,16 +38,16 @@
     environmentFile = config.sops.templates."homepage-dashboard.env".path;
 
     settings = {
-      title = "Hompeage";
+      title = "Homepage";
       theme = "dark";
       layout = {
         "homelab" = {
           style = "row";
-          columns = 2;
+          columns = 3;
         };
         "bookmarks" = {
           style = "row";
-          columns = 2;
+          columns = 3;
         };
       };
     };
@@ -87,6 +92,65 @@
               icon = "borgmatic";
               href = "http://borgbase.com";
               description = "Borg Backups";
+              widget = {
+                type = "customapi";
+                url = "https://api.borgbase.com/graphql";
+                method = "POST";
+                headers = {
+                  Authorization = "Bearer {{HOMEPAGE_VAR_BORG_KEY}}";
+                  Content-Type = "application/json";
+                };
+                requestBody = {
+                  query = ''
+                    {
+                      repoList{
+                        name
+                        lastModified
+                        currentUsage
+                      }
+                    }
+                  '';
+                };
+                display = "list";
+                mappings = [
+                  {
+                    field = "data.repoList.0.currentUsage";
+                    label = "Gitea";
+                    format = "bytes";
+                    scale = 1000000;
+                    additionalField = {
+                      field = "data.repoList.0.lastModified";
+                      label = "Last Backup";
+                      format = "relativeDate";
+                      style = "short";
+                    };
+                  }
+                  {
+                    field = "data.repoList.1.currentUsage";
+                    label = "Owncloud";
+                    format = "bytes";
+                    scale = 1000000;
+                    additionalField = {
+                      field = "data.repoList.1.lastModified";
+                      label = "Last Backup";
+                      format = "relativeDate";
+                      style = "short";
+                    };
+                  }
+                  {
+                    field = "data.repoList.2.currentUsage";
+                    label = "Immich";
+                    format = "bytes";
+                    scale = 1000000;
+                    additionalField = {
+                      field = "data.repoList.2.lastModified";
+                      label = "Last Backup";
+                      format = "relativeDate";
+                      style = "short";
+                    };
+                  }
+                ];
+              };
             };
           }
           {
@@ -115,7 +179,8 @@
                 url = "{{HOMEPAGE_VAR_HA_URL}}";
                 key = "{{HOMEPAGE_VAR_HA_KEY}}";
                 custom = {
-                  "state" = "counter.numero_lavatrici_questo_mese";
+                  template = "{{ states.switch|selectattr('state','equalto','on')|list|length }}";
+                  label = "switches on";
                 };
               };
             };
@@ -143,6 +208,18 @@
                 type = "uptimekuma";
                 url = "{{HOMEPAGE_VAR_UPTIMEKUMA_URL}}";
                 slug = "{{HOMEPAGE_VAR_UPTIMEKUMA_SLUG}}";
+              };
+            };
+          }
+          {
+            "Immich" = {
+              icon = "immich";
+              href = "{{HOMEPAGE_VAR_IMMICH_URL}}";
+              widget = {
+                type = "immich";
+                url = "{{HOMEPAGE_VAR_IMMICH_URL}}";
+                key = "{{HOMEPAGE_VAR_IMMICH_KEY}}";
+                version = 2;
               };
             };
           }
